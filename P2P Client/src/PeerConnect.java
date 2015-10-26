15c8484f284fcf380c028d64804d0fbe50a93535
brewsauce65 <adam.m.brewer@ndsu.edu>
2015-10-26 12:50:03 -0500

    Create PeerConnect.java

---
+++ P2P Client/src/PeerConnect.java
@@ -0,0 +1,46 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class PeerConnect implements Runnable {
+
+	private Socket client;
+	private ServerSocket serverSocket;
+
+/*	public PeerConnect(Socket client) {
+		this.client = client;
+	}*/
+
+	@Override
+	public void run() {
+		System.out.println("Starting the socket server at port:" + 9992);
+		try {
+			serverSocket = new ServerSocket(9992);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Socket client = null;
+
+		while (true) {
+			System.out.println("Waiting for clients...");
+			try {
+				client = serverSocket.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("The following client has connected:" + client.getInetAddress().getCanonicalHostName());
+			// A client has connected to this server. Send welcome message
+			Thread thread = new Thread(new SocketPeerHandler(client));
+			thread.start();
+	}
+	}
+}
+
+	
