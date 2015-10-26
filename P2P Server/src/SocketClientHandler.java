ed934d1f29ed28fb3a4b83919316b5c1bd186fbb
brewsauce65 <adam.m.brewer@ndsu.edu>
2015-10-26 15:22:58 -0500

    Update SocketClientHandler.java

--- P2P Server/src/SocketClientHandler.java
+++ P2P Server/src/SocketClientHandler.java
@@ -1,8 +1,10 @@
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SocketClientHandler implements Runnable {
@@ -16,7 +18,8 @@ public class SocketClientHandler implements Runnable {
 	@Override
 	public void run() {
 		try {
-			System.out.println("Thread started with name:" + Thread.currentThread().getName());
+			System.out.println("Thread started with name:"
+					+ Thread.currentThread().getName());
 			readResponse();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -28,7 +31,8 @@ public class SocketClientHandler implements Runnable {
 		}
 	}
 
-	private void readResponse() throws IOException, InterruptedException, ClassNotFoundException {
+	private void readResponse() throws IOException, InterruptedException,
+			ClassNotFoundException {
 		ObjectInputStream stdIn = new ObjectInputStream(client.getInputStream());
 		String[] clientInput;
 
@@ -47,32 +51,44 @@ public class SocketClientHandler implements Runnable {
 		}
 	}
 
-	private void createClientProfile(String[] input) throws IOException, InterruptedException {
-		SocketServer.addMaster(input);
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+	private void createClientProfile(String[] input) throws IOException,
+			InterruptedException {
+		ArrayList<ArrayList<String>> master = SocketServer.addMaster(input);
+		ObjectOutputStream out = new ObjectOutputStream(
+				client.getOutputStream());
+		out.writeObject(master);
+		
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+				client.getOutputStream()));
 		writer.write("Your files have been added to the directory and are available for download");
 		writer.flush();
 		writer.close();
 	}
 
-	private void appendClientProfile(String[] input) throws IOException, InterruptedException {
+	private void appendClientProfile(String[] input) throws IOException,
+			InterruptedException {
 		SocketServer.appendMaster(input);
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+				client.getOutputStream()));
 		writer.write("Your file list has been updated");
 		writer.flush();
 		writer.close();
 	}
 
-	private void fileSearch(String[] input) throws IOException, InterruptedException {
+	private void fileSearch(String[] input) throws IOException,
+			InterruptedException {
 		String[] id = SocketServer.searchMaster(input);
-		if (id.length>0){
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-		writer.write(id[0]);
-		writer.write(id[1]);
-		writer.flush();
-		writer.close();
+		if (id.length > 0) {
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+					client.getOutputStream()));
+			writer.write(id[0]);
+			writer.newLine();
+			writer.write(id[1]);
+			writer.flush();
+			writer.close();
 		} else {
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+					client.getOutputStream()));
 			writer.write("Your file was not found. Please search again");
 			writer.flush();
 			writer.close();
