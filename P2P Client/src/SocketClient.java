6391628367748dab0aa932547f368b3838e71800
Marc Kostelecky <marc.kostelecky@gmail.com>
2015-10-26 10:41:42 -0500

    pee pee and doo doo

--- P2P Client/src/SocketClient.java
+++ P2P Client/src/SocketClient.java
@@ -1,9 +1,21 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * A Simple Socket client that connects to our socket server
@@ -11,13 +23,97 @@ import java.net.UnknownHostException;
  */
 public class SocketClient {
 
-	private String hostname;
-	private int port;
-	Socket socketClient;
+    private static String hostname = "134.129.91.155";
+    private int port;
+    Socket socketClient;
+    String[] header;
+    String[] names = new String[6];
+    private int peerPort;
+	private String peerName, uploadPath;
+	Socket socket = null;
+	PrintWriter out;
+	BufferedReader in;
+	private JFrame mainFrame;
+	private JPanel panel1;
+    private JButton connectButt, searchButt, changeUpload;
+	private JLabel pName, title;
+	private JTextField enterName;
 
-	public SocketClient(String hostname, int port) {
-		this.hostname = hostname;
+    public SocketClient(String hostname, int port) {
+		//this.hostname = hostname;
 		this.port = port;
+		uploadPath = "C:/Users/Squeelch/Documents/FileStorm";
+		prepareGUI();
+	}
+    
+    private void prepareGUI(){
+		mainFrame = new JFrame("FILESTORMMMMM");
+		panel1 = new JPanel();
+		panel1.setLayout(new FlowLayout());
+		
+		title = new JLabel("FILESTORM CLIENT");
+		
+	
+		title.setLocation(240, 4);
+		title.setFont(title.getFont().deriveFont(32.0f));
+		title.setVisible(true);
+		
+		enterName = new JTextField("Search for File", 32);
+		searchButt = new JButton("Search");
+		connectButt = new JButton("Connect and Upload");
+		pName = new JLabel("Current File Folder: " + uploadPath);
+		changeUpload = new JButton("Change File Folder");
+		
+		connectButt.setLocation(550, 300);
+		
+		enterName.setEnabled(true);
+		//enterName.setLocation(pName.getX() + pName.getWidth() + 10, pName.getY());
+		
+		panel1.add(title);
+		
+		panel1.add(enterName);
+		panel1.add(searchButt);
+		panel1.add(connectButt);
+		panel1.add(pName);
+		panel1.add(changeUpload);
+		mainFrame.setSize(400,200);
+		mainFrame.add(panel1);
+		mainFrame.setVisible(true);
+		
+		connectButt.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e)
+	            {
+	                //Execute when button is pressed
+	                System.out.println("You clicked the button");
+	                try {
+						connect();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	            }
+		});
+		
+		searchButt.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e)
+	            {
+	                //Execute when button is pressed
+	                System.out.println("Searching for files");
+	                names[0] = "SEARCH";
+	                names[1] = hostname;
+	                names[2] = new Integer(port).toString();
+	                names[3] = null;
+	                names[4] = null;
+	                names[5] = null;
+	                try {
+	                	
+						askForTime();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	            }
+		});
 	}
 
 	public void connect() throws UnknownHostException, IOException {
@@ -40,7 +136,8 @@ public class SocketClient {
 	// Successfully passes an array through the socket
 	public void askForTime() throws IOException {
 		//String[] names = { "CREATE", "todd", "8", "yellow"};
-		String[] names = { "SEARCH", "yellow"};
+		//String[] names = {"SEARCH", "yellow"};
+		System.out.println(names[0] + " " + names[1] + " " + names[2]);
 		ObjectOutputStream out = new ObjectOutputStream(socketClient.getOutputStream());
 		out.writeObject(names);
 		// BufferedWriter writer = new BufferedWriter(new
@@ -53,7 +150,7 @@ public class SocketClient {
 	public static void main(String arg[]) {
 		// Creating a SocketClient object
 		SocketClient client = new SocketClient("localhost", 9991);
-		try {
+		/*try {
 			// trying to establish connection to the server
 			client.connect();
 			// asking server for time
@@ -65,6 +162,6 @@ public class SocketClient {
 			System.err.println("Host unknown. Cannot establish connection");
 		} catch (IOException e) {
 			System.err.println("Cannot establish connection. Server may not be up." + e.getMessage());
-		}
-	}
+		} */
+	} 
 }
